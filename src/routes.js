360d59e719295c0d2c87175a1bd48332ef835532
Remy349 <santoraga15@gmail.com>
2024-05-07 08:53:48 -0600

    feat: Added login support for categories.
    Categories can be created.
    Get a list of categories.
    Get a single one.

--- src/routes.js
+++ src/routes.js
@@ -4,18 +4,42 @@ const router = express.Router();
 
 // ---> Categories
 
+const CATEGORIES = [
+  { id: 1, name: "Test category name 1", createdAt: new Date() },
+  { id: 2, name: "Test category name 2", createdAt: new Date() },
+  { id: 3, name: "Test category name 3", createdAt: new Date() },
+];
+
 router.get("/categories", (req, res) => {
-  res.send("List of categories");
+  res.json(CATEGORIES);
 });
 
 router.get("/categories/:id", (req, res) => {
   const categoryId = req.params.id;
 
-  res.send(`Get category by ID: ${categoryId}`);
+  const category = CATEGORIES.find((item) => item.id === Number(categoryId));
+
+  if (!category) {
+    res.json({ message: "Category not found" });
+  }
+
+  res.json(category);
 });
 
 router.post("/categories", (req, res) => {
-  res.send("Create category");
+  const { name } = req.body;
+
+  const id = CATEGORIES.length + 1;
+
+  const newCategory = {
+    id,
+    name,
+    createdAt: new Date(),
+  };
+
+  CATEGORIES.push(newCategory);
+
+  res.json({ message: "Category successfully created" });
 });
 
 router.put("/categories/:id", (req, res) => {
