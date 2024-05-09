c13043cf3b7bf4676825cb2add91aad87035b813
Remy349 <santoraga15@gmail.com>
2024-05-08 23:01:48 -0600

    feat: Added prettier and eslint for code formatting.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-import express from "express";
-import morgan from "morgan";
-import { categoriesRouter } from "./routes/categories.js";
+import express from 'express'
+import morgan from 'morgan'
+import { categoriesRouter } from './routes/categories.js'
 
-const app = express();
+const app = express()
 
-const PORT = 3000 || process.env.PORT;
+const PORT = 3000 || process.env.PORT
 
-app.use(morgan("dev"));
-app.use(express.json());
+app.use(morgan('dev'))
+app.use(express.json())
 
-app.use("/api/categories", categoriesRouter);
+app.use('/api/categories', categoriesRouter)
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,61 +1,61 @@
-import { Router } from "express";
+import { Router } from 'express'
 
-export const categoriesRouter = Router();
+export const categoriesRouter = Router()
 
 const CATEGORIES = [
-  { id: 1, name: "Test category name 1", createdAt: new Date() },
-  { id: 2, name: "Test category name 2", createdAt: new Date() },
-  { id: 3, name: "Test category name 3", createdAt: new Date() },
-];
+  { id: 1, name: 'Test category name 1', createdAt: new Date() },
+  { id: 2, name: 'Test category name 2', createdAt: new Date() },
+  { id: 3, name: 'Test category name 3', createdAt: new Date() }
+]
 
-categoriesRouter.get("/", (req, res) => {
-  res.json(CATEGORIES);
-});
+categoriesRouter.get('/', (req, res) => {
+  res.json(CATEGORIES)
+})
 
-categoriesRouter.get("/:id", (req, res) => {
-  const categoryId = req.params.id;
+categoriesRouter.get('/:id', (req, res) => {
+  const categoryId = req.params.id
 
-  const category = CATEGORIES.find((item) => item.id === Number(categoryId));
+  const category = CATEGORIES.find((item) => item.id === Number(categoryId))
 
   if (!category) {
-    return res.status(404).json({ message: "Category not found" });
+    return res.status(404).json({ message: 'Category not found' })
   }
 
-  res.json(category);
-});
+  res.json(category)
+})
 
-categoriesRouter.post("/", (req, res) => {
-  const { name } = req.body;
+categoriesRouter.post('/', (req, res) => {
+  const { name } = req.body
 
-  const id = CATEGORIES.length + 1;
+  const id = CATEGORIES.length + 1
 
   const newCategory = {
     id,
     name,
-    createdAt: new Date(),
-  };
-
-  CATEGORIES.push(newCategory);
-
-  res.status(201).json({ message: "Category successfully created" });
-});
-
-categoriesRouter.put("/:id", (req, res) => {
-  const categoryId = req.params.id;
-
-  res.send(`Update category by ID: ${categoryId}`);
-});
-
-categoriesRouter.delete("/:id", (req, res) => {
-  const categoryId = req.params.id;
-
-  const category = CATEGORIES.find((item) => item.id === Number(categoryId));
-
-  if (!category) {
-    return res.status(404).json({ message: "Category not found" });
+    createdAt: new Date()
   }
 
-  CATEGORIES.filter((item) => item.id !== category.id);
+  CATEGORIES.push(newCategory)
 
-  res.json({ message: "Category successfully deleted." });
-});
+  res.status(201).json({ message: 'Category successfully created' })
+})
+
+categoriesRouter.put('/:id', (req, res) => {
+  const categoryId = req.params.id
+
+  res.send(`Update category by ID: ${categoryId}`)
+})
+
+categoriesRouter.delete('/:id', (req, res) => {
+  const categoryId = req.params.id
+
+  const category = CATEGORIES.find((item) => item.id === Number(categoryId))
+
+  if (!category) {
+    return res.status(404).json({ message: 'Category not found' })
+  }
+
+  CATEGORIES.filter((item) => item.id !== category.id)
+
+  res.json({ message: 'Category successfully deleted.' })
+})
