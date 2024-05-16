0d08e3fa56c8012721bbee1baac9a8e713feaacd
Remy349 <santoraga15@gmail.com>
2024-05-16 14:11:13 -0600

    feat: Add new server file to handle app entry point and testing packages

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,3 @@
-import 'dotenv/config'
-
 import express from 'express'
 import morgan from 'morgan'
 
@@ -10,8 +8,6 @@ import { authRouter } from './routes/auth'
 
 const app = express()
 
-const PORT = 3000
-
 app.use(morgan('dev'))
 app.use(express.json())
 
@@ -20,6 +16,4 @@ app.use('/api/users', usersRouter)
 app.use('/api/categories', categoriesRouter)
 app.use('/api/posts', postsRouter)
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`)
-})
+export default app
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+
+import app from './app'
+
+const PORT = (process.env.PORT || 3000) as number
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
